81bd4e51f6027b624e0801cca96de24021bc8746
Kek5chen <kx@imkx.dev>
2024-03-05 21:14:03 +0100

    automatically bootstrap lazy.nvim

--- lua/init.lua
+++ lua/init.lua
@@ -1,6 +1,16 @@
 -- Setup lazy.nvim
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
@@ -28,5 +38,7 @@ require('neogit').setup {
 	},
 	kind = "split"
 }
+
+-- he's crazy
 
 print("he's crazy")
