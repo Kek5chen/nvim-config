8aa99e971b516a1cc093b641a23b841a62f80bae
kx <kx@imkx.dev>
2024-05-04 21:50:57 +0200

    changed telescope config

--- lua/init.lua
+++ lua/init.lua
@@ -33,6 +33,7 @@ require("lazy").setup({
 	  "nvim-telescope/telescope.nvim",
     cmd = "Telescope",
   },
+  "junegunn/fzf",
 	"mhinz/vim-startify",
   {
     "Eandrju/cellular-automaton.nvim",
@@ -194,3 +195,39 @@ vim.keymap.set('n', '<Leader>du', function() require('dapui').toggle() end)
 
 print("he's crazy")
 
+-- telescope
+
+require("telescope").setup({
+      defaults = {
+        mappings = {
+          i = {
+            ["<Leader>ff"] = require("telescope.actions").send_to_qflist,
+          },
+        },
+        file_ignore_patterns = { "package-lock.json", "Cargo.lock", "vendor", "node_modules", ".git" },
+
+        -- modifyed ivy
+        sorting_strategy = "ascending",
+        layout_strategy = "bottom_pane",
+        layout_config = {
+          height = 10,
+        },
+        border = false,
+        borderchars = {
+          prompt = { "─", " ", " ", " ", "─", "─", " ", " " },
+          results = { " " },
+          preview = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+        },
+      },
+      extensions = {
+        fzf = {
+          fuzzy = true,
+          override_generic_sorter = false, -- override the generic sorter
+          override_file_sorter = true, -- override the file sorter
+          case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+        },
+        --["ui-select"] = {
+        --require("telescope.themes").get_dropdown {}
+        --}
+      },
+    })
