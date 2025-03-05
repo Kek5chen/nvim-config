cf10bf4e114d002e373ce2151b1efc66eb1414bc
willow <52585984+Kek5chen@users.noreply.github.com>
2025-03-05 19:06:09 +0100

    add telescope config

--- nvim.nix
+++ nvim.nix
@@ -133,6 +133,43 @@
       vim.keymap.set('n', '<F9>', function() require('dap').toggle_breakpoint() end)
       vim.keymap.set('n', '<Leader>du', function() require('dapui').toggle() end)
 
+      -- telescope
+
+      require("telescope").setup({
+        defaults = {
+          mappings = {
+            i = {
+              ["<Leader>ff"] = require("telescope.actions").send_to_qflist,
+            },
+          },
+          file_ignore_patterns = { "package-lock.json", "Cargo.lock", "vendor", "node_modules", ".git" },
+
+          -- modifyed ivy
+          sorting_strategy = "ascending",
+          layout_strategy = "bottom_pane",
+          layout_config = {
+            height = 10,
+          },
+          border = false,
+          borderchars = {
+            prompt = { "─", " ", " ", " ", "─", "─", " ", " " },
+            results = { " " },
+            preview = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+          },
+        },
+        extensions = {
+          fzf = {
+            fuzzy = true,
+            override_generic_sorter = false, -- override the generic sorter
+            override_file_sorter = true, -- override the file sorter
+            case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+          },
+          --["ui-select"] = {
+          --require("telescope.themes").get_dropdown {}
+          --}
+        },
+      })
+
       require('gitsigns').setup()
 
       -- he's crazy
