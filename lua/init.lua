124444c600bf21a40ba71059c08673c76c685fcf
Kek5chen <52585984+Kek5chen@users.noreply.github.com>
2025-03-08 22:21:51 +0100

    change to coq

--- lua/init.lua
+++ lua/init.lua
@@ -19,7 +19,7 @@ require("lazy").setup({
   -- utils
   "nvim-lua/plenary.nvim",
   "ryanoasis/vim-devicons",
-  "nvim-treesitter/nvim-treesitter",
+  -- "nvim-treesitter/nvim-treesitter",
 
 
   -- tools
@@ -88,8 +88,64 @@ require("lazy").setup({
   --   dependencies = { "neovim/nvim-lspconfig" },
   -- },
   -- 'stevearc/conform.nvim',
-  "neoclide/coc.nvim",
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    dependencies = {
+      { "ms-jpq/coq_nvim", branch = "coq" },
+      { "ms-jpq/coq.artifacts", branch = "artifacts" },
+      { "ms-jpq/coq.thirdparty", branch = "3p" },
+      { "nvim-autopairs" }
+    },
+    init = function()
+      local remap = vim.api.nvim_set_keymap
+      local npairs = require('nvim-autopairs')
 
+      npairs.setup({ map_bs = false, map_cr = false })
+
+      vim.g.coq_settings = {
+        keymap = { 
+          recommended = false,
+          bigger_preview = "<c-k>",
+        }, 
+        auto_start = true
+      }
+
+      -- these mappings are coq recommended mappings unrelated to nvim-autopairs
+      remap('i', '<esc>', [[pumvisible() ? "<c-e><esc>" : "<esc>"]], { expr = true, noremap = true })
+      remap('i', '<c-c>', [[pumvisible() ? "<c-e><c-c>" : "<c-c>"]], { expr = true, noremap = true })
+      remap('i', '<tab>', [[pumvisible() ? "<c-n>" : "<tab>"]], { expr = true, noremap = true })
+      remap('i', '<s-tab>', [[pumvisible() ? "<c-p>" : "<bs>"]], { expr = true, noremap = true })
+
+      -- skip it, if you use another global object
+      _G.MUtils= {}
+
+      MUtils.CR = function()
+        if vim.fn.pumvisible() ~= 0 then
+          if vim.fn.complete_info({ 'selected' }).selected ~= -1 then
+            return npairs.esc('<c-y>')
+          else
+            return npairs.esc('<c-e>') .. npairs.autopairs_cr()
+          end
+        else
+          return npairs.autopairs_cr()
+        end
+      end
+      remap('i', '<cr>', 'v:lua.MUtils.CR()', { expr = true, noremap = true })
+
+      MUtils.BS = function()
+        if vim.fn.pumvisible() ~= 0 and vim.fn.complete_info({ 'mode' }).mode == 'eval' then
+          return npairs.esc('<c-e>') .. npairs.autopairs_bs()
+        else
+          return npairs.autopairs_bs()
+        end
+      end
+      remap('i', '<bs>', 'v:lua.MUtils.BS()', { expr = true, noremap = true })
+    end,
+    config = function()
+      -- Your LSP settings here
+    end,
+  },
 
   -- debugging
   "mfussenegger/nvim-dap",
@@ -110,8 +166,9 @@ require "plugins/overseer"
 require "plugins/presence"
 require "plugins/rustacean"
 require "plugins/telescope"
-require "plugins/treesitter"
+-- require "plugins/treesitter"
 require "plugins/gitsigns"
+require "plugins/nvim-autopairs"
 
 print("he's crazy")
 
