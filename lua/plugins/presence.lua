4efe9509e013fd0784485a3da6572716f0f51ede
Kek5chen <52585984+Kek5chen@users.noreply.github.com>
2025-04-14 10:20:51 +0200

    different lyrics

--- lua/plugins/presence.lua
+++ lua/plugins/presence.lua
@@ -1,7 +1,7 @@
 require("presence").setup({
     -- General options
     auto_update         = true,
-    neovim_image_text   = "real",
+    neovim_image_text   = "meow",
     main_image          = "neovim",
     client_id           = "793271441293967371",
     enable_line_number  = true,                      -- Displays the current line number instead of the current project
@@ -9,12 +9,12 @@ require("presence").setup({
     show_time           = true,                       -- Show the timer
 
     -- Rich Presence text options
-    editing_text        = "I can't see like i just got made",
-    file_explorer_text  = "Fuck a plane",
-    git_commit_text     = "Pop a perky, look like im chinese",
-    plugin_manager_text = "Fuck a phone",
-    reading_text        = "I be high with the gods",
-    workspace_text      = "Everything we did was too legitly",
+    editing_text        = "I woke up in my face covered in glitter",
+    file_explorer_text  = "I drop off the earth",
+    git_commit_text     = "Nobody checks upon me",
+    plugin_manager_text = "I like being alone, I don't know why",
+    reading_text        = "Too late, already got too much on my plate",
+    workspace_text      = "Nothing I do is worth a mention",
     line_number_text    = "Line %s out of %s",
 })
 
